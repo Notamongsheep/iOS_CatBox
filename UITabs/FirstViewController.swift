87bf4c9742e7b77bb08a54944f4155a6391100f7
Johnny Chapman <macuser@Johnnys-Macbook-Air.local>
2017-11-06 08:18:18 -0600

    Initial Commit

---
+++ UITabs/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  UITabs
+//
+//  Created by Johnny Chapman on 11/6/17.
+//  Copyright © 2017 Christine Berger. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
